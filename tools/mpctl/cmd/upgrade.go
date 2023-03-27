67d81600792fc1e35a5fbf2855785ce61b84b6aa
cexll <evanxian9@gmail.com>
2023-03-27 15:46:26 +0800

    optimize: cmd upgrade

--- tools/mpctl/cmd/upgrade.go
+++ tools/mpctl/cmd/upgrade.go
@@ -3,7 +3,10 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"io"
+	"net/http"
 	"os/exec"
+	"regexp"
 )
 
 var UpgradeCmd = &cobra.Command{
@@ -21,20 +24,53 @@ func init() {
 }
 
 func upgradeRun(_ *cobra.Command, args []string) {
-	upgradeFunc := func(upgradeCmd string) (err error) {
-		cmd := exec.Command("go", upgradeCmd, cliRepoUrl+"@v"+CLIVersion)
-		fmt.Printf("Upgrade go-mixplus cli: %s\n", cmd.String())
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println("Upgrade go-mixplus cli failed.", err.Error())
-		}
-		return err
+	resp, err := http.Get("https://raw.githubusercontent.com/mix-plus/go-mixplus/master/tools/mpctl/cmd/version.go")
+	if err != nil {
+		panic(err)
 	}
-	if err1 := upgradeFunc("get"); err1 != nil {
-		if err2 := upgradeFunc("install"); err2 != nil {
-			fmt.Println("Upgrade go-mixplus cli failed.")
-		}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println("Upgrade go-mixplus cli failed.")
+		panic(err)
 	}
 
-	fmt.Println(" > ok.")
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	re := regexp.MustCompile(`CLIVersion\s+=\s+"(\d+\.\d+\.\d+)"`)
+	matches := re.FindStringSubmatch(string(body))
+	if len(matches) != 2 {
+		fmt.Println("Could not find version number")
+		return
+	}
+
+	version := matches[1]
+	isUpgrade := VersionCompare(version, CLIVersion)
+	switch isUpgrade {
+	case 0:
+		fmt.Println("The go-mixplus cli is the latest version.")
+	case 1:
+		fmt.Println("Upgrade mpctl to version:", version)
+		upgradeFunc := func(upgradeCmd string) (err error) {
+			cmd := exec.Command("go", upgradeCmd, cliRepoUrl+"@v"+CLIVersion)
+			fmt.Printf("Upgrade go-mixplus cli: %s\n", cmd.String())
+			err = cmd.Run()
+			if err != nil {
+				fmt.Println("Upgrade go-mixplus cli failed.", err.Error())
+			}
+			return err
+		}
+		if err1 := upgradeFunc("get"); err1 != nil {
+			if err2 := upgradeFunc("install"); err2 != nil {
+				fmt.Println("Upgrade go-mixplus cli failed.")
+			}
+		}
+		fmt.Println(" > ok.")
+	case 2:
+		fmt.Println("The go-mixplus cli is a higher version than the latest version.")
+	}
 }
